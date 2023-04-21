3f3dcf6243dafd578c67a321a7fcb18a4411f6b9
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-04-21 09:35:18 +0200

    work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,55 +5,38 @@ import { FindContactForm } from './findContact/FindContactForm';
 import { nanoid } from 'nanoid';
 
 export const App = () => {
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
   const [contacts, setContacts] = useState(() => {
     const dataConst = localStorage.getItem('contacts');
-    if(dataConst) { return JSON.parse(dataConst)}
-    return []
+    if (dataConst) {
+      return JSON.parse(dataConst);
+    }
+    return [];
   });
 
   const [filter, setFilter] = useState('');
 
-  useEffect(()=>{
-    localStorage.setItem("contacts", JSON.stringify(contacts))},[contacts])
- 
-// componentDidMount(){
-//     const dataCont = localStorage.getItem('this.contacts')
-//       if(dataCont !== null){
-//       this.setState({contacts:JSON.parse(dataCont)})
-//     }
-//   } 
-//   componentDidUpdate(_, prevState) {
-//     if(this.state.contacts !== prevState.contacts){
-//       localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
-//     }
-//   }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = data => {
-    if(contacts.find(item =>item.name === data.name))
-    {return alert(`Name ${data.name} is already in contacts!`)} 
+    if (contacts.find(item => item.name === data.name)) {
+      return alert(`Name ${data.name} is already in contacts!`);
+    }
     const newContact = {
       id: nanoid(),
       ...data,
     };
     setContacts(prevState => [...prevState, newContact]);
-    };
-    
+  };
+
   const deleteContact = id => {
-    setContacts((prevState) => {
+    setContacts(prevState => {
       return prevState.filter(contact => contact.id !== id);
-    })
-     };
+    });
+  };
   const onChangeFind = ev => {
-    setFilter(ev.currentTarget.value.toLowerCase())
+    setFilter(ev.currentTarget.value.toLowerCase());
   };
   const filteredContacts = () => {
     if (filter) {
@@ -64,27 +47,30 @@ export const App = () => {
       return contacts;
     }
   };
-      return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <AddContactForm addContact={addContact} />
-        <h2>Contacts</h2>
-        <FindContactForm onChangeFind={onChangeFind} />
-        <ContactList
-          contacts={filteredContacts()}
-          onDeleteContact={deleteContact}
-        />
-      </div>
-    );
-  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <AddContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <FindContactForm onChangeFind={onChangeFind} />
+      <ContactList
+        contacts={filteredContacts()}
+        onDeleteContact={deleteContact}
+      />
+    </div>
+  );
+};
 
-
+// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
